8852e459985e0ed3b8a64b4d106ef3aba099c64f
ramalber <ramarigal@yahoo.com>
2025-12-07 05:58:35 -0500

    Updated the source code for Final Project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,48 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+
+    const total = cart.foreach(item => item.quantity * parseFloat(item.cost.substring(1)));
+    return(total);
+    
   };
 
   const handleContinueShopping = (e) => {
+
+    onContinueShopping(e);
    
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1)
+    {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
+    else
+    {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+
+    dispatch(removeItem(item.name));
+
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    
+    const cost = parseFloat(item.cost.substring(1)) * item.quantity;
+
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
